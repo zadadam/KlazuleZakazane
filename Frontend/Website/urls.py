3b9a8cd51ee28b6d20c9fb42d697aa301d1f0a8c
Pawel <p.posielezny@usos.edu.pl>
2016-09-24 19:09:09 +0200

    Menu added to the website

--- Frontend/Website/urls.py
+++ Frontend/Website/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'info', views.info, name='info'),
+    url(r'contact', views.contact, name='contact')
 ]
