69c23a965476b1f9c909f5f4d05dca8cec6d968e
Pawel <p.posielezny@usos.edu.pl>
2016-09-25 11:05:36 +0200

    unnecessary files deleted

--- Frontend/Website/helper.py
+++ Frontend/Website/helper.py
@@ -7,9 +7,6 @@ def handle_uploaded_text_file(f):
     return result
 
 def handle_uploaded_pdf_file(f):
-    # with open('name2.pdf', 'wb+') as destination:
-    #     for chunk in f.chunks():
-    #         destination.write(chunk)
     reader = PdfFileReader(f)
     contents = ''
     for i in range(reader.numPages):
